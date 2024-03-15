bda9ca1959e4c16babec886744be72a2b5b48d53
jasurbek-mannonovgit config --global user.email jasurbekmannonov2020@gmail.comgit config -lgit clone https://github.com/jasurbek-mannonov/mern-blog-web-app.gitgit clone https://github.com/jasurbek-mannonov/mern-blog-web-app.gitgit config --global user.name jasurbek-mannonov <jasurbekmannonov2020@gmail.com>
2024-03-16 00:39:40 +0500

    add basic restaurant: add form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,57 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+interface Restaurant {
+  name?: string,
+  address?: string,
+  status?: RestaurantStatus,
+  dishes?: Dish[]
+}
+
+type RestaurantStatus = 
+| 'Want To Try' | "Recommended" | "Do Not Recommended" | "Must Try"
+
+const statusList = [
+  'Want to try',
+  'Recommended',
+  'Do Not Recommended',
+  'Must Try'
+]
+
+
+const restaurantList = ref<Restaurant[]>([]);
+const newRestaurant = ref<Restaurant>({});
+
+function addRestaurant() {
+  restaurantList.value.push({
+    name: newRestaurant.value.name,
+    status: 'Want To Try',
+    dishes: []
+  })
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <pre>
+      {{ newRestaurant }}
+    </pre>
+    <form @submit.prevent="addRestaurant">
+      <div>
+        <label for="restaurant-name">Restaurant name</label>
+        <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
+      </div>
+      <div>
+        <label for="restaurant-status">Restaurant status</label>
+        <select name="restaurant-status" id="restaurant-status" v-model="newRestaurant.status">
+          <option v-for="status in statusList" :value="status" :key="status">{{status}}</option>
+        </select>
+      </div>
+      <button type="submit">Add Restaurant</button>
+    </form>
+
+    <ul>
+      <li v-for="restaurant in restaurantList" :key="restaurant">{{ restaurant.name }}</li>
+    </ul>
   </main>
 </template>
